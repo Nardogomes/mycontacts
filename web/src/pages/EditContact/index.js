671fc0067abd737462e2c34eb70e68fd597dfce6
Ednardo Gomes <ednardo@gomes.com>
2023-06-16 10:35:31 -0300

    Criando o componente ContactForm.

--- web/src/pages/EditContact/index.js
+++ web/src/pages/EditContact/index.js
@@ -1,7 +1,12 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Editar Nardo Gomes" />
+    <>
+      <PageHeader title="Editar Nardo Gomes" />
+
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
   );
 }
--- web/src/pages/NewContact/index.js
+++ web/src/pages/NewContact/index.js
@@ -1,20 +1,12 @@
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
-import Button from '../../components/Button';
+import ContactForm from '../../components/ContactForm';
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
 
-      <Input type="text" placeholder="Nome" />
-
-      <Select>
-        <option value="instagram">Instagram</option>
-      </Select>
-
-      <Button type="button">Salvar contato</Button>
+      <ContactForm buttonLabel="Cadastrar" />
     </>
   );
 }
