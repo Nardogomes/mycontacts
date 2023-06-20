a96a996e7b7bc6c4c735f537bcbe0544ba91e208
Ednardo Gomes <ednardo@gomes.com>
2023-06-20 12:19:11 -0300

    Configurando status de danger no Modal.

--- web/src/components/Modal/index.js
+++ web/src/components/Modal/index.js
@@ -1,11 +1,13 @@
+import PropTypes from 'prop-types';
+
 import Button from '../Button';
 
 import { Overlay, Container, Footer } from './styles';
 
-export default function Modal() {
+export default function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Título do modal</h1>
         <p>
           Corpo do modal
@@ -13,7 +15,7 @@ export default function Modal() {
 
         <Footer>
           <button type="button" className="cancel-button">Cancelar</button>
-          <Button type="button">
+          <Button type="button" danger={danger}>
             Deletar
           </Button>
         </Footer>
@@ -21,3 +23,11 @@ export default function Modal() {
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
